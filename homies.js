141fe2b53eb92b8365a2b187359f6cdb30168c8e
Alex Glover <alex.glover@kellogg.northwestern.edu>
2021-03-17 08:25:53 -0500

    add home page and auth event listener

--- homies.js
+++ homies.js
@@ -94,20 +94,67 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     // Hide the form when signed-out
     document.querySelector('form').classList.add('hidden')
+    
+    // clear the page when signed out
+    document.querySelector('.posts').innerHTML = ``
 
-    // Initializes FirebaseUI Auth
-    let ui = new firebaseui.auth.AuthUI(firebase.auth())
+    // provide the home page code fr index.html, including a let's get started button
+    document.querySelector('.posts').insertAdjacentHTML('beforeend', `
 
-    // FirebaseUI configuration
-    let authUIConfig = {
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      signInSuccessUrl: 'index.html'
-    }
+      <div class="w-full h-screen flex">
+        <img src="https://images.unsplash.com/photo-1614590354333-c72a209c78a5?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDQyfFJfRnluLUd3dGx3fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="background" class="object-cover object-center h-screen w-7/12">
+        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <div class="sm:text-center lg:text-left">
+            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <span class="block xl:inline">Home</span>
+              <span class="block text-indigo-900 xl:inline">Sweet Home</span>
+            </h1>
+            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              Whether you're looking to design or your dream home, scroll through beautiful rooms, or provide design advice, we're all homies here! Submit your design needs, crowdsource advice from our design community, and see which idea wins! 
+            </p>
+            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div class="rounded-md shadow">
+                <button class="getstarted-button bg-indigo-900 hover:bg-blue-600 text-white px-4 py-2 rounded-xl">Let's get started!</button>
+                
+              </div>
+            </div>
+      </div>
+    `)
 
-    // Starts FirebaseUI Auth
-    ui.start('.sign-in-or-sign-out', authUIConfig)
+    // only show the auth flow if the let's get started button is clicked. this makes a more-friendly user experience
+    document.querySelector('.getstarted-button').addEventListener('click', function(event) {
+        console.log('lets get started clicked')
+      
+        // Initializes FirebaseUI Auth
+        let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+        // FirebaseUI configuration
+        let authUIConfig = {
+          signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+          ],
+          signInSuccessUrl: 'index.html'
+        }
+
+        // Starts FirebaseUI Auth
+        ui.start('.sign-in-or-sign-out', authUIConfig)
+
+    })
+
+
+    // // Initializes FirebaseUI Auth
+    // let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+    // // FirebaseUI configuration
+    // let authUIConfig = {
+    //   signInOptions: [
+    //     firebase.auth.EmailAuthProvider.PROVIDER_ID
+    //   ],
+    //   signInSuccessUrl: 'index.html'
+    // }
+
+    // // Starts FirebaseUI Auth
+    // ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 })
 
